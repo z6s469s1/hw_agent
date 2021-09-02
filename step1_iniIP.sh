6cd6a8ba5d3237c362f4eca21d2a173bb842b633
FFs <47924485+z6s469s1@users.noreply.github.com>
2021-09-02 16:59:45 +0800

    Create step1_iniIP.sh

---
+++ step1_iniIP.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ `id -u` -ne 0 ]; then
+   echo "need root permission!"
+   exit 1
+fi
+
+#requirement
+sudo apt-get upgrade
+sudo apt-get update --allow-releaseinfo-change
+sudo apt-get install gedit -y
+
+#ip setting
+sudo rm /etc/dhcpcd.conf
+sudo cp conf/dhcpcd.conf /etc/dhcpcd.conf
+
+sudo reboot
