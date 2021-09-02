b3b7c9b863903607b2f79fd5acdc91d79c810504
FFs <47924485+z6s469s1@users.noreply.github.com>
2021-09-02 16:41:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ sudo apt-get install gedit -y
 #ip setting
 sudo rm /etc/dhcpcd.conf
 sudo cp conf/dhcpcd.conf /etc/dhcpcd.conf
+
 sudo ifconfig eth1 192.168.2.1 netmask 255.255.255.0
 
 #ip forwarding
@@ -18,7 +19,11 @@ sudo sysctl -w net.ipv4.ip_forward=1
 
 #dhcp setting
 sudo apt install isc-dhcp-server -y
+
 sudo rm /etc/dhcp/dhcpd.conf
 sudo cp conf/dhcpd.conf /etc/dhcp/dhcpd.conf
-sudo systemctl restart isc-dhcp-server 
 
+sudo rm /etc/default/isc-dhcp-server
+sudo cp conf/isc-dhcp-server  /etc/default/isc-dhcp-server
+
+sudo systemctl restart isc-dhcp-server 
